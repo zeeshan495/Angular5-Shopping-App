7a28b33a5cd9aae7fb35ac1feab3c0c8844c8ecd
shaikzeeshan88 <shaikzeeshan88@gmail.com>
2018-10-04 00:32:51 +0530

    first commit

---
+++ src/app/header/header.component.html
@@ -0,0 +1,1 @@
+<h2>i'm header</h2>
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component(
+    {
+        selector : 'app-header',
+        templateUrl : "../header/header.component.html"
+    }
+)
+export class HeaderComponent {
+
+}
