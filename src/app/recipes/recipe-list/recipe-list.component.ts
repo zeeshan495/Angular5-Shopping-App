876d0aae4c1cbe1af09125b37a8ad76ab509ad71
shaikzeeshan88 <shaikzeeshan88@gmail.com>
2018-10-11 23:48:31 +0530

    components_completed

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes:Recipe[] = [
-    new Recipe('chicken','Tasty with protien','https://www.cbc.ca/food/content/images/recipes/KoreanChicken.jpg')
+    new Recipe('chicken','Tasty with protien','https://www.cbc.ca/food/content/images/recipes/KoreanChicken.jpg'),
+    new Recipe('Maggie','Fast Food and Tasty','https://cdn-a.william-reed.com/var/wrbm_gb_food_pharma/storage/images/publications/food-beverage-nutrition/foodnavigator-asia.com/article/2017/11/29/nestle-hit-by-new-maggi-noodle-setback-in-india/7591781-1-eng-GB/Nestle-hit-by-new-Maggi-noodle-setback-in-India_wrbm_large.jpg')
   ];
   constructor() { }
 
