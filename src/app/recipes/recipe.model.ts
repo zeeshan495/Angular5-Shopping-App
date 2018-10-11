4ec0f491b321c95e3c75f4cc49f88beba7a92340
shaikzeeshan88 <shaikzeeshan88@gmail.com>
2018-10-11 20:33:41 +0530

    added recipe

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,10 @@
+export class Recipe {
+public name: String;
+public description: String;
+public imagepath: String;
+constructor(name:string,desc:string,image:String){
+this.name=name;
+this.description=desc;
+this.imagepath=image;
+}
+}
